0555ef79d2e62647f1b183eaee5cf3c433e76bfd
Jay Cisneros <jaycmeza@icloud.com>
2024-01-19 15:07:25 -0800

    1st update to app.js file

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Application ready');
+function writeDate() {
+    const today = new Date();
+    const formatter = new Intl.DateTimeFormat('en-us', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        hour12: true
+    });
+    const dateEl = document.getElementById('date');
+    dateEl.textContent = formatter.format(today);
+}
+
+writeDate();
